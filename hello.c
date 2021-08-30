8935e7d97ae4fff29c0d8f087f0d17fe9d671781
Calvin Dao <cdao2@unl.edu>
2021-08-30 01:35:36 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Calvin Dao
+ *         cdao2@unl.edu
+ * Date: 2021/08/25
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Calvin Dao!\n");
+
+  return 0;
+}
